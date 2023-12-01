fce77d576dfde22f2e9f3bdb15f017f1a68ff8f6
Farsonic <fp@prowsehouse.com>
2023-12-01 13:43:55 +1000

    correcte removal of user from docker group

--- uninstall_elk.sh
+++ uninstall_elk.sh
@@ -42,6 +42,6 @@ fi
 # Revert system changes
 sudo sed -i '/vm.max_map_count=262144/d' /etc/sysctl.conf
 sudo sysctl -w vm.max_map_count=$(cat /proc/sys/vm/max_map_count)
-sudo usermod -G $(groups $(whoami) | sed 's/ /\,/g' | sed 's/,docker//g' | cut -d: -f2 | sed 's/^ //') $(whoami)
+sudo usermod -G $(groups $(whoami) | sed -e 's/^.*: //' -e 's/ /\,/g' | tr ',' '\n' | grep -v docker | tr '\n' ',' | sed 's/,$//') $(whoami)
 
 echo "Uninstallation completed!"
