ac6b445e8e54d1b5bdd0d89c5c94a7e4c74739c0
Francois Prowse <fp@prowsehouse.com>
2024-03-11 11:04:15 +1000

    Update install_elk.sh

--- install_elk.sh
+++ install_elk.sh
@@ -26,16 +26,18 @@ if [ -d "pensando-elk" ]; then
     exit 1
 fi
 
+#Remove any old Docker versions 
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
 # Install required tools and Docker
 if ! command -v docker &>/dev/null; then
     sudo apt-get update
-    sudo apt-get install -y ca-certificates curl gnupg
+    sudo apt-get install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
-    #sudo chmod a+r /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update
+    sudo apt-get update
     sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin jq
 fi
 
