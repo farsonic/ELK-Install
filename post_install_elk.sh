a63ba25516f3611d3de9c6f8bb59889995cbe99d
Francois Prowse <fp@prowsehouse.com>
2024-03-11 14:28:45 +1000

    Update post_install_elk.sh

--- post_install_elk.sh
+++ post_install_elk.sh
@@ -56,9 +56,9 @@ fi
 
 read -p "Do you want to perform DNS Lookups? (y/n): " CONFIGURE_DNS
 if [[ "$CONFIGURE_DNS" == "y" ]]; then
-    read -p "Enter your DNS Server IPs (Commar seperated): " DNS_SERVERS
+    #read -p "Enter your DNS Server IPs (Single Host only): " DNS_SERVERS
     sed -i "s/EF_PROCESSOR_ENRICH_IPADDR_DNS_ENABLE: 'false'/EF_PROCESSOR_ENRICH_IPADDR_DNS_ENABLE: 'true'/" docker-compose.yml
-    sed -i "s/EF_PROCESSOR_ENRICH_IPADDR_DNS_NAMESERVER_IP: ''/EF_PROCESSOR_ENRICH_IPADDR_DNS_NAMESERVER_IP: '$DNS_SERVERS'/" docker-compose.yml
+    #sed -i "s/EF_PROCESSOR_ENRICH_IPADDR_DNS_NAMESERVER_IP: ''/EF_PROCESSOR_ENRICH_IPADDR_DNS_NAMESERVER_IP: '$DNS_SERVERS'/" docker-compose.yml
 fi
 
 read -p "Do you want to install RiskIQ Components? (y/n): " INSTALL_RISKIQ
